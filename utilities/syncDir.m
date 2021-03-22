077839af2795bc7d44ae03317c91f07227c20e16
Diego A Pacheco Pinedo <dpacheco0921@gmail.com>
2021-03-22 11:17:00 -0400

    fix bugs

--- utilities/syncDir.m
+++ utilities/syncDir.m
@@ -1,15 +1,20 @@
-function syncDir(iDir, oDir, i_serverId, o_serverId)
+function command2submit = syncDir(iDir, oDir, log_serverId, ...
+    i_serverId, o_serverId, openterminal, config_dir)
 % syncDir: Function to syncronize folders within servers
 %
 % Usage:
-%   syncDir(iDir, oDir, i_serverId, o_serverId)
+%   command2submit = syncDir(iDir, oDir, i_serverId, ...
+%      o_serverId, openterminal, config_dir)
 %
 % Args:
 %   iDir: source dir (in local server/cluster)
 %   oDir: target dir (in target server/cluster)
+%   log_serverId: server ID to which you connect (apps, spock, della, tigress)
 %   i_serverId: source server ID (apps, spock, della, tigress)
 %   o_serverId: target server ID (apps, spock, della, tigress)
 %       (needs to be defined in ssh_config)
+%   openterminal: flag to open terminal
+%   config_dir: directory of ssh_config file to use for passwordless login to cluster
 %
 % Example of bucket/scratch/tigress/della directories
 % '/jukebox/scratch/', '/jukebox/murthy/'
@@ -23,6 +28,10 @@ function syncDir(iDir, oDir, i_serverId, o_serverId)
 % To do:
 %   maybe include overwrite: cp -r
 
+if ~exist('log_serverId', 'var') || isempty(log_serverId)
+    log_serverId = 'apps';
+end
+
 if ~exist('i_serverId', 'var') || isempty(i_serverId)
     i_serverId = 'apps';
 end
@@ -31,20 +40,47 @@ if ~exist('o_serverId', 'var') || isempty(o_serverId)
     o_serverId = 'apps';
 end
 
+if ~exist('openterminal', 'var') || isempty(openterminal)
+    openterminal = 0;
+end
+
+if ~exist('config_dir', 'var') || isempty(config_dir)
+    
+    if ispc
+        config_dir = ['c:/Users/', ...
+            getenv('USERNAME'), '/.ssh/ssh_config'];
+    elseif ismac
+        config_dir = [];
+    end
+    
+end
+
 [~, username, ~, ~, ~, ~, host_name] = user_defined_directories;
 
-eval(['username = username.', i_serverId]);
+eval(['username = username.', log_serverId]);
+
+% target server
+inputSerDir = '';
+
+if ~isempty(log_serverId) && ~strcmp(log_serverId, i_serverId)
+    selidx = contains(host_name, i_serverId);
+    inputSerDir = [username, host_name{selidx}];
+end
 
 % target server
 targetSerDir = '';
 
-if ~isempty(i_serverId) && ~strcmp(i_serverId, o_serverId)
-    selidx = contains(host_name, i_serverId);
-    targetSerDir = host_name{selidx};
+if ~isempty(log_serverId) && ~strcmp(log_serverId, o_serverId)
+    selidx = contains(host_name, o_serverId);
+    targetSerDir = [username, host_name{selidx}];
 end
 
 % server to log in
-sshCo = ['ssh ', i_serverId, ' '];
+if ~isempty(config_dir)
+    sshCo = ['ssh ', log_serverId, ' -F ', config_dir, ' '];
+else
+    sshCo = ['ssh ', log_serverId, ' '];
+end
 
 % Submit command
 % v: verbose
@@ -52,10 +88,21 @@ sshCo = ['ssh ', i_serverId, ' '];
 % h: human readable numbers
 % z: compress
 % r: recursive
+% --delete: delete extraneous files from destination dirs
 
-CommandStr{1} = [sshCo, '''rsync -avhzr ', iDir, ' ', targetSerDir, oDir, ''''];
+%command2submit = [sshCo, '"rsync -avhzr ', iDir, ' ', targetSerDir, oDir, '"'];
+command2submit = [sshCo, '"rsync -avhzr --delete ', inputSerDir, iDir, ' ', targetSerDir, oDir, '"'];
+
 initT = tic;
-coexecuter(CommandStr)
+
+if openterminal == 1
+    eval(['!', command2submit, ' &'])
+elseif openterminal == 0
+    coexecuter(command2submit)
+else
+    fprintf('Generate command (not executed)')
+end
+
 toc(initT)
 
 end
